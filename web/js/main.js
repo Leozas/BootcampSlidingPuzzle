6a65169f7477ad0cf828740bd897a5bd2dea7e91
Michael Loyselle <52831149+Leozas@users.noreply.github.com>
2019-10-01 16:09:19 -0400

    UI Update .001 - Foundationals

--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,24 @@
 //structure = var, then defined func, then overall func
+/*
+divApp.appendChild( newrow(),
+                    newrow(
+                        newcol(
+                            newrow(
+                                newcol(), newcol(),newcol()
+                            newrow(
+                                newcol(), newcol(),newcol()
+                            newrow(
+                                newcol(), newcol(),newcol()
+                            )
+                        newcol(
+                            newrow(victory text)
+                            newrow(turn text)
+                            newrow(score text)
+                            newrow(
+                                newbtn(reset)
+                                newbtn(start)
+                                newbtn(rules)
+                            )
+                        )
+                    newrow()
+                    */
